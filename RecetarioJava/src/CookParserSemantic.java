a005a79fc7b5122301157628352f0692265dd6d1
Nicolas Mura <nicolas.mura37@gmail.com>
2018-07-31 15:26:16 -0400

    EMPIEZA LO DIFICIL

---
+++ RecetarioJava/src/CookParserSemantic.java
@@ -0,0 +1,114 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class CookParserSemantic extends CookParserBaseVisitor<Object>{
+
+	protected Map<String, String> _vars = new HashMap<String, String>();
+
+	public CookParserSemantic() {}
+
+	@Override
+    public Object visitIniciar_cocina(CookParserParser.Iniciar_cocinaContext ctx) { 
+	    	if(ctx.PREPARARINGREDIENTES().getText().equals("COMMENCER_LA_RECETTE"))    {
+	    		System.out.println("Para comenzar la receta se necesitan:");
+			System.out.println(" ");
+
+		}
+	    return null;
+	}
+	
+	@Override
+	public Object visitFinalizar_cocina(CookParserParser.Finalizar_cocinaContext ctx) {
+	    	if(ctx.FINCOCINA().getText().equals("ACHEVEMENT_RECETTE"))    {
+	    		System.out.println("ULALAAAA!!");
+			System.out.println("TASTY");
+		}
+	    return null;
+	}
+	
+	@Override
+	public Object visitDeclaration(MileParserParser.DeclarationContext ctx) {
+		String var_type = ctx.variable_type().getText();
+        String id = ctx.ID().getText();
+
+        if (!_vars.containsKey(id)) {
+        		var_type = getVarType(var_type);
+        		_vars.put(id, var_type);
+        		System.out.println(String.format("\t%s %s;", var_type, id));
+        } else {
+        		throw new IllegalArgumentException("Variable '" + id + "' already exist");
+        }
+        
+		return null;
+	}
+
+	@Override
+	public Object visitRead(MileParserParser.ReadContext ctx) {
+		String id = ctx.ID().getText();
+		if (_vars.containsKey(id)) {
+			System.out.println(String.format("\tscanf(\"%s\", &%s);", getVarTypeMode(_vars.get(id)), id));
+		} else {
+    			throw new IllegalArgumentException("Variable '" + id + "' doesn't defined");
+		}
+		return null; 
+	}
+	
+	@Override
+	public Object visitPrint(MileParserParser.PrintContext ctx) {
+		if (ctx.ID().size() > 0) {
+			String id, format = "", args = "";
+			for (int i = 0; i < ctx.ID().size(); i++) {
+				id = ctx.ID(i).getText();
+				if (_vars.containsKey(id)) {
+					format += getVarTypeMode(_vars.get(id)) + " ";
+					args += id + ", ";
+				} else {
+	    				throw new IllegalArgumentException("Variable '" + id + "' doesn't defined");
+				}
+			}
+			System.out.println(String.format("\tprintf(\"%s\", %s);", format.substring(0, format.length() - 1), args.substring(0, args.length() - 2)));
+		} else {
+			String text = ctx.STRING().getText();
+			if(text != null) {
+				System.out.println(String.format("\tprintf(%s);", text));
+			}
+		}
+		return null;  
+	}
+	
+	@Override
+	public Object visitIf_block(MileParserParser.If_blockContext ctx) {
+		// Completar
+		return null; 
+	}
+	
+	private String getVarType(String var_type) {
+		if(var_type.equals("entero") || var_type.equals("logico"))
+			return "int";
+		else if(var_type.equals("real"))
+			return "float";
+		else
+			return "char";
+	}
+	
+	private String getVarTypeMode(String var_type) {
+		if(var_type.equals("int"))
+			return "%d";
+		else if(var_type.equals("real"))
+			return "%f";
+		else
+			return "%s";
+	}
+	
+	private String replace(String stat) {
+		stat.replace("=", "==");
+		stat.replace("<>", "!=");
+		stat.replace("AND", "&&");
+		stat.replace("OR", "&&");
+		return stat;
+	}
+}
